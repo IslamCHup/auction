31b3f38bdbf862c707c7d7bb2da612469f73b269
IslamCHup <80688364+IslamCHup@users.noreply.github.com>
2026-01-14 03:34:34 +0300

    сделал основные middlewares, теперь начинаю тз2

--- gateway/cmd/app/main.go
+++ gateway/cmd/app/main.go
@@ -1,40 +1,46 @@
 package main
 
 import (
+	"gateway/internal/config"
 	"gateway/internal/middleware"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
+	"gateway/internal/proxy"
 	"os"
-	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func makeProxyHandler(proxy *httputil.ReverseProxy) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Request.URL.Path = c.Param("proxyPath")
-		proxy.ServeHTTP(c.Writer, c.Request)
-	}
-}
-
 func main() {
-	remote, err := url.Parse(os.Getenv("AUCTION_SERVICE_URL"))
-	if err != nil {
-		panic(err)
+	logger := config.InitLogger()
+
+	if err := godotenv.Load(); err != nil {
+		logger.Error("env не найдено", "err", err.Error())
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-
-	proxy.Transport = &http.Transport{
-		ResponseHeaderTimeout: 500 * time.Millisecond,
-	}
+	auctionProxy := proxy.NewReverseProxy(os.Getenv("AUCTION_SERVICE_URL"), logger)
+	userProxy := proxy.NewReverseProxy(os.Getenv("USER_SERVICE_URL"), logger)
+	walletProxy := proxy.NewReverseProxy(os.Getenv("WALLET_SERVICE_URL"), logger)
 
 	r := gin.Default()
-	
+
+	r.Use(cors.Default())
+	r.Use(middleware.RateLimitMiddleware())
 	r.Use(middleware.TimeoutMiddleware())
 
-	r.Any("/*proxyPath", makeProxyHandler(proxy))
+	r.Any("/auction/*path", proxy.MakeProxyHandler(auctionProxy))
+	r.Any("/user/*path", proxy.MakeProxyHandler(userProxy))
+	r.Any("/wallet/*path", proxy.MakeProxyHandler(walletProxy))
 
-	r.Run(":8080")
+	logger.Info(
+		"starting gateway",
+		"addr", ":8080",
+		"auction", os.Getenv("AUCTION_SERVICE_URL"),
+		"user", os.Getenv("USER_SERVICE_URL"),
+		"wallet", os.Getenv("WALLET_SERVICE_URL"),
+	)
+
+	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
+		logger.Error("failed to run server", "err", err.Error())
+	}
 }
